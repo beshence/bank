d11357b9c0fe64a46dd67374fc51e86c64859298
Vyacheslav Bytsko <169024760+vyacheslavbytsko@users.noreply.github.com>
2026-07-26 05:21:22 +0300

    we now can provide CA certificate with proof of owning it

---
+++ internal/api/endpoints/misc/ca.go
@@ -0,0 +1,94 @@
+package misc
+
+import (
+	"bank/internal/api"
+	"bank/internal/settings"
+
+	"crypto/hmac"
+	"crypto/sha3"
+	"encoding/base64"
+	hashlib "hash"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CAV1(deps *api.Dependencies) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if deps == nil || deps.DB == nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": "UNKNOWN",
+			})
+			return
+		}
+
+		var body struct {
+			Ciphertext string `json:"ciphertext"`
+		}
+
+		if err := c.ShouldBindJSON(&body); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": "INVALID_JSON",
+			})
+			return
+		}
+
+		ciphertext, err := base64.RawURLEncoding.DecodeString(
+			body.Ciphertext,
+		)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": "INVALID_CIPHERTEXT",
+			})
+			return
+		}
+
+		dk, err := settings.GetBankDecapsulationKey(deps.DB)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": "NO_KEY",
+			})
+			return
+		}
+
+		sharedSecret, err := dk.Decapsulate(ciphertext)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"err": "INVALID_KEM",
+			})
+			return
+		}
+
+		caPEM := settings.GetBankCACertPEM(deps.DB)
+
+		hash := sha3.Sum256(
+			[]byte(caPEM),
+		)
+
+		mac := hmac.New(
+			func() hashlib.Hash {
+				return sha3.New256()
+			},
+			sharedSecret,
+		)
+
+		mac.Write([]byte(
+			"BESHENCE_BANK_CA_V1",
+		))
+
+		mac.Write(hash[:])
+
+		proof := mac.Sum(nil)
+
+		c.JSON(http.StatusOK, gin.H{
+			"err": "0",
+			"ca":  caPEM,
+			"proof": base64.RawURLEncoding.EncodeToString(
+				proof,
+			),
+		})
+	}
+}
